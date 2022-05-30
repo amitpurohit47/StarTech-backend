d322a2a1a7f2f6ce21b44c1ef8d93b0ffff4c766
mayursarode <sarodemayur55@gmail.com>
2022-05-31 00:58:37 +0530

    addAttendance Function Completed

--- src/controllers/Attendance.js
+++ src/controllers/Attendance.js
@@ -6,6 +6,19 @@ const addAttendence = async (req, res) => {
       const student = Student.findOne({ _id: req.body.students[i] });
       //find j in attendence i.e. req.teacher._id matches with teacherId of j.
       //add date to attendence array
+      var temp=student.attendance;
+      for(var j=0; j < temp.length; j++) {
+        if(temp[j].teacherId==req.teacher._id)
+        {
+         break;
+        }
+      }
+      if(j==temp.length)
+      {
+        res.status(500).send({ error: "Teacher Id Not Found!" });
+        return;
+      }
+      student.attendance[j].attendence.push(req.date);
       await student.save();
     }
     res.status(201).send("Attendence updated");
