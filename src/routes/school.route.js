ce51dfbf247158c1436ff5b2740ac7abd4f7a485
Amit Purohit <amitpurohit4747@gmail.com>
2022-06-05 23:14:52 +0530

    query added

--- src/routes/school.route.js
+++ src/routes/school.route.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { schoolAuth } from "../middleware";
-import { createSchool, loginSchool } from "../controllers";
+import { allNotices, createSchool, loginSchool, addQuery, allQueries } from "../controllers";
 import { School } from "../models";
 
 const SchoolRouter = express.Router();
 
 SchoolRouter.post("/signup", createSchool);
-
 SchoolRouter.get("/login", schoolAuth, loginSchool);
+SchoolRouter.get("/notice", allNotices);
+SchoolRouter.post("/query", addQuery);
+SchoolRouter.get("/query", allQueries);
 
 export { SchoolRouter };
